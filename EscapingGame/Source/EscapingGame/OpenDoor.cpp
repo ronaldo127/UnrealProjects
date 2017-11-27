1e932be3e5a427a1339eefba4251accfb7fd2b0b
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2017-11-27 17:46:00 -0500

    My first script based animation in unreal. My script opens a door.

---
+++ EscapingGame/Source/EscapingGame/OpenDoor.cpp
@@ -0,0 +1,39 @@
+// Copyright Ronaldo Felipe 2017
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	owner = GetOwner();
+	rotator = owner->GetActorRotation();
+	rotator.ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *rotator.ToString());
+	speed = 30.0f;
+	minAngle = -60.0f;
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (rotator.Yaw >= minAngle) {
+		rotator.Add(0, -speed * DeltaTime, 0);
+		owner->SetActorRotation(rotator);
+	}
+}
+
