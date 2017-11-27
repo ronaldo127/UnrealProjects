7b4ac070f238c55ac536fe42bbcd9bfffaf7f331
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2017-11-26 20:21:59 -0500

    Hello world of unreal components!

--- EscapingGame/Source/EscapingGame/PositionReport.cpp
+++ EscapingGame/Source/EscapingGame/PositionReport.cpp
@@ -10,7 +10,6 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -19,7 +18,7 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Position report is alive!!"));
 	
 }
 
